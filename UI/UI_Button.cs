b0e284eadd89ecaea641a9e0c6d5de6097dd20e2
Curookie <curookie@naver.com>
2023-11-01 01:55:41 +0900

    WIP : Framework DEV 2

--- UI/UI_Button.cs
+++ UI/UI_Button.cs
@@ -6,7 +6,7 @@ namespace GameFrameworks
 {
     public enum AnimType { NONE, SCALE_PUNCH,  }
 
-    public class UI_Button : Button
+    public class UI_Button : Button, IBaseUI
     {
         public AnimType animType = AnimType.NONE;
     }
